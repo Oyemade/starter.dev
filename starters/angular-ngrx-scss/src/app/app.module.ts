1c6b894c44aa60cfacb130a6bdcf0e237726e0ae
Jamie Kuppens <jamie.kuppens@thisdot.co>
2022-04-08 10:39:50 -0700

    feat(ngrx): implement routing for ngrx starter

--- starters/angular-ngrx-scss/src/app/app.module.ts
+++ starters/angular-ngrx-scss/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import ButtonComponent from './counter-button/button.component';
+import ButtonComponent from './button-example/counter-button/button.component';
+import { ButtonExampleComponent } from './button-example/button-example.component';
+import { FetchExampleComponent } from './fetch-example/fetch-example.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent],
+  declarations: [AppComponent, ButtonComponent, ButtonExampleComponent, FetchExampleComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
